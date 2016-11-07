287335a2ddc26c6c5b5ac7f537294b9e53bec492
PreymeTeyme <mlpreymann@gmail.com>
2016-11-07 18:07:22 +0100

    Update brainfq.h

--- brainfq.h
+++ brainfq.h
@@ -1,3 +1,33 @@
+
+
+ /* Name:             brainfq.h                                                                             */
+ /* Author:           Matthias Preymann, HTBL-Hollabrunn                                                    */
+ /* Date:             7.11.2016                                                                             */
+ /* Description:      A hobbyist implementation of a brainfuck compiler and interpreter based on bytecode   */
+ /*                   there is no real purpose of existence                                                 */
+
+ /* Version:          1.2                                                                                   */
+ /* Additional files: main.c, load.c, memory.c, parse.c, parsing_functions.c, run.c, save_bytecode.c,       */
+ /*                   sprint_bytecode.c                                                                     */
+
+ /* Disclaimer:       "This software is provided as it is and the author does not take any responsibility   */
+ /*                   for any kind of damage caused by the usage of this code. Copying, sharing and the     */
+ /*                   usage in any commercial or none commercial way is explicitly allowed."                */
+
+ /* Usage:            The program expects at least one start argument containing the path of the source     */
+ /*                   file to compile / run. If there is a second start argument 'debug' or 'debug!' the    */
+ /*                   debug mode is enabled and a ton of additional information about compiling and during  */
+ /*                   run time is spammed into the console.                                                 */
+ /*                   If the file type of the source file is not '.bfq' the compiler expects a basic or     */
+ /*                   advanced brainfuck source file, which is compiled to bytecode, saved and run.         */
+ /*                   Bfq-files are just opened and run.                                                    */
+ /*                   Except commenting the compiler is fully capable of the basic brainfuck standard. The  */
+ /*                   difference in commenting results, since the compiler interprets everything including  */
+ /*                   characters and numbers. These are part of advanced brainfq. A normal comment is       */
+ /*                   by a '?' infront. This declared everything until the next '?' or '\n' to be a comment.*/
+ /*                   For further information how to use advanced brainfq feel free to check my GitHub page */
+ /*                   out.                                                                                  */
+
 #ifndef BRAINFQ_H_INCLUDED
 #define BRAINFQ_H_INCLUDED
 
@@ -7,25 +37,25 @@
 #include <string.h>
 #include <stdbool.h>
 
-extern const char cmd_array[9];
+extern const char cmd_array[9];         //all available commands
 
 
-int loadsourcefl(char **const read, const char *const flname);
-int loadbrainfqfl(char ***const code, const char *const flname, int *const funamount);
-bool addtosource(char **read, int *const sourcesize, const char cursor);
+int loadsourcefl(char **const read, const char *const flname);                          //loads a source file
+int loadbrainfqfl(char ***const code, const char *const flname, int *const funamount);  //loads a bytecode file
+bool addtosource(char **read, int *const sourcesize, const char cursor);                //adds characters in 16 byte steps to a source file
 
-int parse(uint8_t ***const code, const char *const source, const int sourcesize);
-int save_bytecode(const char *const flname, const uint8_t *const *const code, const int funamount);
-void sprint_bytecode(const uint8_t *const *const code, const int funamount);
-int run(const uint8_t *const *const code, int *const pointerpos, const int curfunction, const int funamount, const bool debug);
+int parse(uint8_t ***const code, const char *const source, const int sourcesize);                                               //compiles a source file to bytecode
+int save_bytecode(const char *const flname, const uint8_t *const *const code, const int funamount);                             //writes bytecode to file
+void sprint_bytecode(const uint8_t *const *const code, const int funamount);                                                    //prints bytecode to console
+int run(const uint8_t *const *const code, int *const pointerpos, const int curfunction, const int funamount, const bool debug); //runs the brainfuck bytecode
 
-int *getelement(const int position);
-int getquadbyte(const uint8_t *const code, int *const pos);
-unsigned int getmemsize();
+int *getmemcell(const int position);        //get the value of a memory cell or generate one if it does not exist yet
+unsigned int getmemsize();                  //get current amount of allocated memory cells
 
-uint8_t repaeting(const char *const code, int *const pos, const int codesize, const char op);
-bool newbyte(uint8_t **const memory, int *const cmdsize);
-bool newquadbyte(uint8_t **const memory, int *const cmdsize);
+uint8_t repaeting(const char *const code, int *const pos, const int codesize, const char op);   //counts the amount of repeating commands in a source file
+bool newbyte(uint8_t **const memory, int *const cmdsize);                                       //generates a new byte in a string
+bool newquadbyte(uint8_t **const memory, int *const cmdsize);                                   //generates four new bytes in a string
+int getquadbyte(const uint8_t *const code, int *const pos);                                     //retrieves four bytes from a string and merges them into an integer
 
 #endif // BRAINFQ_H_INCLUDED
 
