75212952ca8fb6f0bd226462f567a0fb2da0d052
PreymeTeyme <mlpreymann@gmail.com>
2016-11-07 18:08:56 +0100

    Update load.c

--- load.c
+++ load.c
@@ -8,31 +8,31 @@ int loadsourcefl(char **const read, const char *const flname)
 
     *read= NULL;
 
-    FILE *sourcefl= fopen(flname, "r");
+    FILE *sourcefl= fopen(flname, "r");                         //open source file in read only mode
 
-    if(sourcefl==NULL)
+    if(sourcefl==NULL)                                          //check for file error
     {
         printf("[@Error] Source file not found.\n");
         return 0;
     }
 
-    while(fscanf(sourcefl, "%c", &cursor)!=EOF)
+    while(fscanf(sourcefl, "%c", &cursor)!=EOF)                 //iterate through all characters in the file
     {
-        if(cursor=='?')
+        if(cursor=='?')                                         //begin or end of a comment
         {
             incomment= !incomment;
         }
 
-        if(cursor=='\n')
+        if(cursor=='\n')                                        //end of comment at the end of a line
         {
             incomment= false;
         }
 
-        if(!incomment)
+        if(!incomment)                                          //check if inside a comment
         {
-            if((cursor!=' ')&&(cursor!='\n')&&(cursor!='\t'))
+            if((cursor!=' ')&&(cursor!='\n')&&(cursor!='\t'))   //add only relevant characters to the source string
             {
-                if(!addtosource(read, &sourcesize, cursor))
+                if(!addtosource(read, &sourcesize, cursor))     //check for memory allocation error
                 {
                     return 0;
                 }
@@ -40,16 +40,16 @@ int loadsourcefl(char **const read, const char *const flname)
         }
     }
 
-    fclose(sourcefl);
+    fclose(sourcefl);                                           //close file and return amount of read commands
     return sourcesize;
 }
 
 
 bool addtosource(char **read, int *const sourcesize, const char cursor)
 {
-    if((*sourcesize)%16==0)
+    if((*sourcesize)%16==0)                                         //check if the end of another 16byte block is reached
     {
-        *read= realloc(*read, ((*sourcesize)+16) * sizeof(char));
+        *read= realloc(*read, ((*sourcesize)+16) * sizeof(char));   //allocate another 16 bytes and check for memory allocation error
         if(*read==NULL)
         {
             printf("[@Error] Memory allocation error, for new cmd string element.\n");
@@ -57,7 +57,7 @@ bool addtosource(char **read, int *const sourcesize, const char cursor)
         }
     }
 
-    (*read)[*sourcesize]= cursor;
+    (*read)[*sourcesize]= cursor;                                   //set new byte
 
     (*sourcesize)++;
     return true;
@@ -72,29 +72,29 @@ int loadbrainfqfl(char ***const code, const char *const flname, int *const funam
     *code= NULL;
     *funamount=0;
 
-    FILE *sourcefl= fopen(flname, "rb");
+    FILE *sourcefl= fopen(flname, "rb");                                    //open brainfq file in binary read only mode
 
-    if(sourcefl==NULL)
+    if(sourcefl==NULL)                                                      //check for file error
     {
         printf("[@Error] Brainfq file not found.\n");
         return 0;
     }
 
-    while(fscanf(sourcefl, "%c", &cursor)==1)
+    while(fscanf(sourcefl, "%c", &cursor)==1)                               //iterate through all bytes in the file
     {
-        if(jmp>=0)
+        if(jmp>=0)                                                          //only interpret commands
         {
-            if(cursor==9)
+            if(cursor==9)                                                   //check for function indicator
             {
                 infunction= !infunction;
 
-                if(infunction)
+                if(infunction)                                              //if new function is reached
                 {
                     (*funamount)++;
 
-                    *code= realloc(*code, (*funamount) * sizeof(uint8_t*));
+                    *code= realloc(*code, (*funamount) * sizeof(uint8_t*)); //allocate new function array
 
-                    if(*code!=NULL)
+                    if(*code!=NULL)                                         //check for memory allocation error
                     {
                         (*code)[(*funamount)-1]= NULL;
                     }
@@ -105,12 +105,12 @@ int loadbrainfqfl(char ***const code, const char *const flname, int *const funam
                     }
                 }
 
-                if(!addtosource(code[(*funamount)-1], &sourcesize, cursor))
+                if(!addtosource(code[(*funamount)-1], &sourcesize, cursor)) //add function indicator
                 {
                     return 0;
                 }
 
-                if(!infunction)
+                if(!infunction)                                             //add end of string symbol at end of function array
                 {
                     if(!addtosource(code[(*funamount)-1], &sourcesize, '\0'))
                     {
@@ -122,13 +122,13 @@ int loadbrainfqfl(char ***const code, const char *const flname, int *const funam
             {
 
 
-                if(infunction)
+                if(infunction)                                              //only interpret commands inside a function; may be used to save meta info inside the byte code file
                 {
-                    if((cursor==5)||(cursor==6))
+                    if((cursor==5)||(cursor==6))                            //if the command expects another data 4 bytes (integer)
                     {
-                        jmp=-5;
+                        jmp=-5;                                             //set the amount of bytes to not interpret, since they are data bytes
                     }
-                    else if((cursor>=1)&&(cursor<=4))
+                    else if((cursor>=1)&&(cursor<=4))                       //if command expects another data byte
                     {
                         jmp=-2;
                     }
@@ -143,7 +143,7 @@ int loadbrainfqfl(char ***const code, const char *const flname, int *const funam
         }
         else
         {
-            if(!addtosource(code[(*funamount)-1], &sourcesize, cursor))
+            if(!addtosource(code[(*funamount)-1], &sourcesize, cursor))     //add data bytes to function array
             {
                 return 0;
             }
@@ -152,7 +152,13 @@ int loadbrainfqfl(char ***const code, const char *const flname, int *const funam
         jmp++;
     }
 
-    fclose(sourcefl);
+    if(jmp<0)                                                               //check for missing data bytes
+    {
+        printf("[@Error] Corrupted bytecode file.\n");
+        printf("[@Error] Expected at least another '%d' bytes of code.\n", jmp * (-1) );
+    }
+
+    fclose(sourcefl);                                                       //close file
     return sourcesize;
 }
 
