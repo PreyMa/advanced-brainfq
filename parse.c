9fe4fb3f2e8816656f1ff965b1406ba5d2fa853b
PreymeTeyme <mlpreymann@gmail.com>
2016-11-07 18:10:45 +0100

    Update parse.c

--- parse.c
+++ parse.c
@@ -7,19 +7,19 @@ int parse(uint8_t ***const code, const char *const source, const int sourcesize)
 
     *code= NULL;
 
-    for(int i=0; i<sourcesize; i++)
+    for(int i=0; i<sourcesize; i++)                                                             //iterate through source file
     {
-        if(source[i]==':')
+        if(source[i]==':')                                                                      //check for function indicator
         {
             infunction=!infunction;
 
-            if(infunction)
+            if(infunction)                                                                      //if new function is reached
             {
                 funamount++;
 
-                *code= realloc(*code, funamount * sizeof(uint8_t*));
+                *code= realloc(*code, funamount * sizeof(uint8_t*));                            //generate new function array
 
-                if(*code!=NULL)
+                if(*code!=NULL)                                                                 //check for allocation error
                 {
                     (*code)[funamount-1]= NULL;
                 }
@@ -30,13 +30,13 @@ int parse(uint8_t ***const code, const char *const source, const int sourcesize)
                 }
             }
 
-            if(!newbyte(&((*code)[funamount-1]), &cmdamount))
+            if(!newbyte(&((*code)[funamount-1]), &cmdamount))                                   //add function indicator to function array
             {
                 return 0;
             }
             (*code)[funamount-1][cmdamount-1]= 9;
 
-            if(!infunction)
+            if(!infunction)                                                                     //add end of string symbol to end of function array
             {
                 if(!newbyte(&((*code)[funamount-1]), &cmdamount))
                 {
@@ -47,23 +47,22 @@ int parse(uint8_t ***const code, const char *const source, const int sourcesize)
         }
         else
         {
-            if(infunction)
+            if(infunction)                                                                      //if currently in a function
             {
-                switch(source[i])
+                switch(source[i])                                                               //enter main state machine
                 {
 
-                case '>':
-                    //if(!newbyte(&((*code)[funamount-1]), &cmdamount))
-                    if(!newbyte(code[funamount-1], &cmdamount))
+                case '>':                                                                       //new pointer operator
+                    if(!newbyte(code[funamount-1], &cmdamount))                                 //add command
                     {
                         return 0;
                     }
                     (*code)[funamount-1][cmdamount-1]= 1;
-                    if(!newbyte(&((*code)[funamount-1]), &cmdamount))
+                    if(!newbyte(&((*code)[funamount-1]), &cmdamount))                           //add data byte
                     {
                         return 0;
                     }
-                    (*code)[funamount-1][cmdamount-1]= repaeting(source, &i, sourcesize, '>');
+                    (*code)[funamount-1][cmdamount-1]= repaeting(source, &i, sourcesize, '>');  //count the amount commands
                     i--;
                     break;
 
@@ -109,79 +108,52 @@ int parse(uint8_t ***const code, const char *const source, const int sourcesize)
                     i--;
                     break;
 
-                case '[':
-                    if(!newbyte(&((*code)[funamount-1]), &cmdamount))
+                case '[':                                                                       //new loop
+                    if(!newbyte(&((*code)[funamount-1]), &cmdamount))                           //add command byte
                     {
                         return 0;
                     }
                     (*code)[funamount-1][cmdamount-1]= 5;
 
-                    loop_depth++;
+                    loop_depth++;                                                               //add loop stack element
                     loop_stack= (int*) realloc(loop_stack, loop_depth * sizeof(int));
-                    if(loop_stack==NULL)
+                    if(loop_stack==NULL)                                                        //check for allocation error
                     {
                         printf("[@Error] Memory allocation error, for new loop stack element during parsing.\n");
                         return 0;
                     }
 
-                    //printf("In stack [: %d\n", cmdamount-1);
-                    loop_stack[loop_depth-1]= cmdamount-1;
+                    loop_stack[loop_depth-1]= cmdamount-1;                                      //push begin of the loop to the loop stack
 
-                    if(!newquadbyte(&((*code)[funamount-1]), &cmdamount))
+                    if(!newquadbyte(&((*code)[funamount-1]), &cmdamount))                       //add data integer
                     {
                         return 0;
                     }
                     break;
 
-                case ']':
-                    //printf("Hinter [: %d\n", cmdamount);
-                    (*code)[funamount-1][loop_stack[loop_depth-1]+1]= ((uint8_t*)&cmdamount)[0];
+                case ']':                                                                       //end of loop
+                    (*code)[funamount-1][loop_stack[loop_depth-1]+1]= ((uint8_t*)&cmdamount)[0];//save end position of loop to begin of loop command
                     (*code)[funamount-1][loop_stack[loop_depth-1]+2]= ((uint8_t*)&cmdamount)[1];
                     (*code)[funamount-1][loop_stack[loop_depth-1]+3]= ((uint8_t*)&cmdamount)[2];
                     (*code)[funamount-1][loop_stack[loop_depth-1]+4]= ((uint8_t*)&cmdamount)[3];
 
-
-                    /*int test=0;
-
-                    ((uint8_t*)&test)[0]= (*code)[funamount-1][4];
-                    ((uint8_t*)&test)[1]= (*code)[funamount-1][5];
-                    ((uint8_t*)&test)[2]= (*code)[funamount-1][6];
-                    ((uint8_t*)&test)[3]= (*code)[funamount-1][7];
-
-                    printf("Hinter [: %d\n", test);*/
-
-                    if(!newbyte(&((*code)[funamount-1]), &cmdamount))
+                    if(!newbyte(&((*code)[funamount-1]), &cmdamount))                           //add command byte
                     {
                         return 0;
                     }
                     (*code)[funamount-1][cmdamount-1]= 6;
 
-                    //printf("Nr: %d\n", cmdamount);
-
-                    if(!newquadbyte(&((*code)[funamount-1]), &cmdamount))
+                    if(!newquadbyte(&((*code)[funamount-1]), &cmdamount))                       //add data integer
                     {
                         return 0;
                     }
 
-                    //printf("zeigt zu: %d\n", (*code)[funamount-1][test]);
-
-                    (*code)[funamount-1][cmdamount-4]= ((uint8_t*)(loop_stack+loop_depth-1))[0];
+                    (*code)[funamount-1][cmdamount-4]= ((uint8_t*)(loop_stack+loop_depth-1))[0];//save begin position of loop to end of loop command
                     (*code)[funamount-1][cmdamount-3]= ((uint8_t*)(loop_stack+loop_depth-1))[1];
                     (*code)[funamount-1][cmdamount-2]= ((uint8_t*)(loop_stack+loop_depth-1))[2];
                     (*code)[funamount-1][cmdamount-1]= ((uint8_t*)(loop_stack+loop_depth-1))[3];
 
-                    /*
-                    printf("Nr: %d : %d\n", cmdamount, i);
-
-
-                    ((uint8_t*)&test)[0]= (*code)[funamount-1][29];
-                    ((uint8_t*)&test)[1]= (*code)[funamount-1][30];
-                    ((uint8_t*)&test)[2]= (*code)[funamount-1][31];
-                    ((uint8_t*)&test)[3]= (*code)[funamount-1][32];
-
-                    printf("Hinter ]: %d\n", test);*/
-
-                    loop_depth--;
+                    loop_depth--;                                                               //pop stack
                     loop_stack= (int*) realloc(loop_stack, loop_depth * sizeof(int));
                     break;
 
@@ -201,7 +173,7 @@ int parse(uint8_t ***const code, const char *const source, const int sourcesize)
                     (*code)[funamount-1][cmdamount-1]= 8;
                     break;
 
-                default:
+                default:                                                                        //unknown state machine value
                     printf("[@Error] Character out of context: '%c'.\n", source[i]);
                     break;
                 }
